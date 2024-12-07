138d685e784d3fff84f9125eaa36cf25758b7525
matin-karshenas <karshnasmtyn@gmail.com>
2024-12-07 23:24:09 +0330

    Initial commit

---
+++ Login_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=">
+    <link rel="stylesheet" href="Web_css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="Login">
+        <input type="text" name="UserName" required>
+    </div>
+</body>
+</html>
---
+++ eduaction.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Web_css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Website</title>
+</head>
+<body>
+    <?php
+     include("index2.html");
+    ?>
+
+    <section class="Home">
+        
+
+        <div class="home_content">
+            <h1>
+               My <span>Education</span>
+            </h1>
+
+            <h3 class="typing-text">
+                I have 200 bachelor
+            </h3>
+            <p>
+            Donald John Trump (born June 14, 1946) is an American politician, media personality, and businessman who became the president-elect of the United States after winning the 2024 presidential election. He previously served as the 45th president of the United States from 2017 to 2021
+            </p>
+
+            <?php
+                include("DATAS.html");
+            ?>
+        </div>
+    </section>
+</body>
+</html>
